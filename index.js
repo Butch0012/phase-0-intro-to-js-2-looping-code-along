07e80c3b4379e6d3a241b70a14e5a826b1479271
Elnathan Mburu <elnathanmburu@gmail.com>
2023-10-08 09:12:47 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+function writeCards(names, occasion) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${occasion} gift!`;
+      thankYouMessages.push(message);
+    }
+    return thankYouMessages;
+  }
+  
+  // Function to count down from a number and log each number to the console
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
